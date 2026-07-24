550283359b131370c9939e65f0a7521e7c0ac06e
aoladosu <goladosurahman@gmail.com>
2026-07-24 08:25:47 +0000

    Remove matplotlib import for faster Streamlit Cloud loading

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from typing import List, Optional
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import plotly.express as px
 import streamlit as st
